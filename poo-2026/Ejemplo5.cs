e2004aa64c2670dc28ecce8170a92fc0361bdf08
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2026-04-06 10:06:33 -0300

    Interfase

---
+++ poo-2026/Ejemplo5.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace poo_2026;
+public class Ejemplo5
+{
+    //public Ejemplo5()
+    //{
+        //PersonaBD pBD = new PersonaBD();
+        //pBD.ObtenerPesona(1);
+        //Console.WriteLine("-------------------------");
+
+        //PersonaLista pLista = new PersonaLista();
+        //pLista.ObtenerPesona(2);
+        //Console.WriteLine("-------------------------");
+    //}
+    
+    public void EjecutarBuscarPersona(IBuscarPersona bp, int id)
+    {
+        bp.ObtenerPersona(id);
+    }
+
+    public IBuscarPersona EjecutarBuscarPersona(TipoBusqueda tipo)
+    {
+        switch (tipo)
+        {
+            case TipoBusqueda.BaseDatos:
+                PersonaBD bp = new PersonaBD();
+                return bp;
+            case TipoBusqueda.Lista:
+                PersonaLista bpl = new PersonaLista();
+                return bpl;
+            default:
+                throw new ArgumentException("Tipo de búsqueda no válido");
+        }
+    }
+}
+
+
+//CLASE RESPONSABLE DE OBTENER PERSONAS DESDE LA BASE DE DATOS
+public class PersonaBD : IBuscarPersona
+{
+    public void ObtenerPersona(int id)
+    {
+        // Simulamos la obtención de una persona desde la base de datos
+        Console.WriteLine($"Obteniendo persona id = {id} desde la base de datos...");
+    }
+}
+
+//CLASE RESPONSABLE DE OBTENER PERSONAS DESDE UNA LISTA
+public class PersonaLista : IBuscarPersona
+{
+    public void ObtenerPersona(int id)
+    {
+        // Simulamos la obtención de una persona desde la base de datos
+        Console.WriteLine($"Obteniendo persona id = {id} desde una lista...");
+    }
+}
+
+//Interfase
+public interface IBuscarPersona
+{
+    void ObtenerPersona(int id);
+}
+
+public enum TipoBusqueda
+{
+    BaseDatos,
+    Lista
+}
+
+
+
+
+
--- poo-2026/Program.cs
+++ poo-2026/Program.cs
@@ -6,5 +6,22 @@
 
 //Ejemplo3 ej = new Ejemplo3();
 
-Ejemplo4 ej = new Ejemplo4();
+//Ejemplo4 ej = new Ejemplo4();
 
+Ejemplo5 ej = new Ejemplo5();
+
+//ej.EjecutarBuscarPersona(new PersonaBD(), 1);
+//PersonaBD pBD = new PersonaBD();
+//ej.EjecutarBuscarPersona(pBD, 1);
+//Console.WriteLine("-------------------------");
+
+//ej.EjecutarBuscarPersona(new PersonaLista(), 2);
+//Console.WriteLine("-------------------------");
+
+IBuscarPersona buscadorBD = ej.EjecutarBuscarPersona(TipoBusqueda.BaseDatos);
+buscadorBD.ObtenerPersona(1);
+Console.WriteLine("-------------------------");
+
+var buscadorLista = ej.EjecutarBuscarPersona(TipoBusqueda.Lista);
+buscadorLista.ObtenerPersona(1);
+Console.WriteLine("-------------------------");
