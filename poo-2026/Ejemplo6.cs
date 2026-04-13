fd07e7e276736b76ed3feaa86aeca22ecacb1640
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2026-04-12 21:55:05 -0300

    Clases y metodos static, extensiones

---
+++ poo-2026/Ejemplo6.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poo_2026
+{
+    public class Ejemplo6
+    {
+        //public static void PruebaStatic()
+        //{
+        //    Console.WriteLine("Ejemplo 6 metodo static");
+        //}
+        //public void PruebaNoStatic()
+        //{
+        //    Console.WriteLine("Ejemplo 6 metodo NO static");
+        //}
+
+        public void MetodoConError()
+        {
+            int res = 0;
+            int a = 10;
+            try
+            {
+                res = a / 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Util.MensajeError("", e, true, true));
+            }
+        }
+
+        public void CalculoEdad()
+        {
+            Console.WriteLine(Util.CalcularEdad(new DateTime(1990, 5, 15)));
+            Console.WriteLine("------------------------------------");
+
+            var fecha = new DateTime(1990, 5, 15);
+            Console.WriteLine($"Edad con extensiion: {fecha.CalcularEdad()}");
+            Console.WriteLine("------------------------------------");
+        }
+    }
+
+    public static class Util
+    {
+        public static string MensajeError(string mensaje="",
+                                          Exception? e = null,
+                                          bool conTrace = false,
+                                          bool conInner = false)
+        {
+            var msg = mensaje;
+            if (e != null)
+            {
+                if (msg != "")
+                {
+                    msg = msg + "\r\n";
+                }
+                msg = $"{msg} Mensaje: {e.Message}";
+
+                if (conTrace)
+                {
+                    msg = $"{msg} \r\n Trace: {e.StackTrace}";
+                }
+
+                if (conInner && e.InnerException != null)
+                {
+                    msg = $"{msg} \r\n Inner: {e.InnerException.Message}";
+                }
+            }
+
+            return msg;
+        }
+
+        public static int CalcularEdad(this DateTime fechaNacimiento,
+                                       DateTime? fecha = null)
+        {
+            var fechaCalculo = (fecha ?? DateTime.UtcNow).Date;
+            var nacimiento = fechaNacimiento.ToUniversalTime().Date;
+            var edad = fechaCalculo.Year - fechaNacimiento.Year;
+            if (fechaNacimiento > fechaCalculo.AddYears(-edad)) edad--;
+            return edad;
+        }
+
+    }
+}
--- poo-2026/Program.cs
+++ poo-2026/Program.cs
@@ -8,7 +8,7 @@
 
 //Ejemplo4 ej = new Ejemplo4();
 
-Ejemplo5 ej = new Ejemplo5();
+//Ejemplo5 ej = new Ejemplo5();
 
 //ej.EjecutarBuscarPersona(new PersonaBD(), 1);
 //PersonaBD pBD = new PersonaBD();
@@ -18,10 +18,21 @@ Ejemplo5 ej = new Ejemplo5();
 //ej.EjecutarBuscarPersona(new PersonaLista(), 2);
 //Console.WriteLine("-------------------------");
 
-IBuscarPersona buscadorBD = ej.EjecutarBuscarPersona(TipoBusqueda.BaseDatos);
-buscadorBD.ObtenerPersona(1);
-Console.WriteLine("-------------------------");
+//IBuscarPersona buscadorBD = ej.EjecutarBuscarPersona(TipoBusqueda.BaseDatos);
+//buscadorBD.ObtenerPersona(1);
+//Console.WriteLine("-------------------------");
 
-var buscadorLista = ej.EjecutarBuscarPersona(TipoBusqueda.Lista);
-buscadorLista.ObtenerPersona(1);
-Console.WriteLine("-------------------------");
+//var buscadorLista = ej.EjecutarBuscarPersona(TipoBusqueda.Lista);
+//buscadorLista.ObtenerPersona(1);
+//Console.WriteLine("-------------------------");
+
+Ejemplo6 ej = new Ejemplo6();
+
+    //Ejemplo6.PruebaStatic();
+
+    //ej.PruebaNoStatic();
+
+//ej.MetodoConError();
+ej.CalculoEdad();
+
+
